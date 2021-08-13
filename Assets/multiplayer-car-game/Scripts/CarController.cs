1b42dfc887a4f9f87aed504530aa69180a4603d7
Yeray Candel <ycansam@epsg.upv.es>
2021-08-13 16:26:20 +0200

    Added RPM

--- Assets/multiplayer-car-game/Scripts/CarController.cs
+++ Assets/multiplayer-car-game/Scripts/CarController.cs
@@ -9,12 +9,24 @@ public class CarController : NetworkBehaviour
     enum Traccion { Trasera, Delantera, n4x4 };
     [SerializeField] Transform cameraTransform;
     [SerializeField] Traccion traccion;
-    
+
 
     [Header("Motor")]
     [SerializeField] private float motorForce = 50;
     [SerializeField] private float breakForce;
     [SerializeField] private float maxSteerAngle = 30;
+
+    [SerializeField] private float[] GearRatio;
+    private int currentGear = 0;
+    private float engineRPM;
+    [SerializeField] private float maxEngineRPM;
+    [SerializeField] private float minEngineRPM;
+    [SerializeField] private float maxSpeed;
+
+
+
+
+
 
     [Header("Ruedas")]
     [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
@@ -22,30 +34,49 @@ public class CarController : NetworkBehaviour
 
     [SerializeField] private Transform frontLeftWheelTransform, frontRightWheeTransform;
     [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
+
+
+
+
+
     Rigidbody rb;
     private float horizontalInput;
     private float verticalInput;
     private float steeringAngle;
+
+    private void OnGUI()
+    {
+
+        if (IsLocalPlayer)
+        {
+            GUILayout.Label("KPH: " + rb.velocity.magnitude * 3.6f);
+            GUILayout.Label("RPM: " + engineRPM);
+            GUILayout.Label("GEAR: " + GearRatio[currentGear]);
+        }
+    }
     private void Start()
     {
-        if(!IsLocalPlayer){
+        if (!IsLocalPlayer)
+        {
             cameraTransform.GetComponent<AudioListener>().enabled = false;
             cameraTransform.GetComponent<Camera>().enabled = false;
-        }else{
+        }
+        else
+        {
             rb = GetComponent<Rigidbody>();
         }
     }
     private void FixedUpdate()
     {
-        
+
         if (IsLocalPlayer)
         {
             GetInput();
             Steer();
             Accelerate();
             UpdateWheelPoses();
+            Brakes();
         }
-
     }
     public void GetInput()
     {
@@ -58,17 +89,43 @@ public class CarController : NetworkBehaviour
         frontLeftWheelCollider.steerAngle = steeringAngle;
         frontRightWheelCollider.steerAngle = steeringAngle;
     }
+    private void Brakes()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            frontLeftWheelCollider.brakeTorque = breakForce;
+            frontRightWheelCollider.brakeTorque = breakForce;
+            rearLeftWheelCollider.brakeTorque = breakForce;
+            rearRightWheelCollider.brakeTorque = breakForce;
+        }
+        else
+        {
+            frontLeftWheelCollider.brakeTorque = 0;
+            frontRightWheelCollider.brakeTorque = 0;
+            rearLeftWheelCollider.brakeTorque = 0;
+            rearRightWheelCollider.brakeTorque = 0;
+        }
+
+    }
     private void Accelerate()
     {
-        if (Traccion.Delantera == traccion)
+        if (traccion == Traccion.Delantera)
         {
             frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
             frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+
+            engineRPM = GetEngineRPM(frontLeftWheelCollider, frontRightWheelCollider);
+            ShiftGears();
         }
-        if (Traccion.Trasera == traccion)
+        if (traccion == Traccion.Trasera)
         {
             rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
             rearRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+
+            engineRPM = GetEngineRPM(rearLeftWheelCollider, rearRightWheelCollider);
+            ShiftGears();
         }
         if (traccion == Traccion.n4x4)
         {
@@ -76,10 +133,54 @@ public class CarController : NetworkBehaviour
             frontRightWheelCollider.motorTorque = verticalInput * motorForce;
             rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
             rearRightWheelCollider.motorTorque = verticalInput * motorForce;
+            engineRPM = GetEngineRPM(frontLeftWheelCollider, frontRightWheelCollider, rearLeftWheelCollider, rearRightWheelCollider);
+            ShiftGears();
+        }
+    }
+
+    private float GetEngineRPM(WheelCollider left_whl, WheelCollider right_whl, WheelCollider left_whl_2 = null, WheelCollider right_whl_2 = null)
+    {
+        if (left_whl_2 != null)
+        {
+            return (left_whl.rpm + right_whl.rpm + left_whl_2.rpm + right_whl_2.rpm) / 4 * GearRatio[currentGear];
+        }
+        else
+        {
+            return (left_whl.rpm + right_whl.rpm) * GearRatio.Length / GearRatio[currentGear];
+        }
+    }
+    private void ShiftGears()
+    {
+        if (engineRPM >= maxEngineRPM)
+        {
+            if (engineRPM * GearRatio[currentGear] > maxEngineRPM)
+            {
+                if (currentGear < GearRatio.Length - 1)
+                {
+                    currentGear++;
+                    engineRPM = minEngineRPM;
+                }
+
+            }
         }
 
+        if (engineRPM <= minEngineRPM)
+        {
+            if (engineRPM * GearRatio[currentGear] < minEngineRPM)
+            {
+                if (currentGear > 1)
+                {
+                    currentGear--;
+                    engineRPM = maxEngineRPM;
+                }
 
+            }
+        }
     }
+
+
+
+    // wheels positions
     private void UpdateWheelPoses()
     {
         UpdateWheelPose(frontLeftWheelCollider, frontLeftWheelTransform);
